b2b11c2eae02053179d11efde03af25daf15a8e6
Matthias Lischka <matthias.lischka@gmx.at>
2021-10-21 20:20:56 +0200

    add faunadb connection function

--- netlify/functions/log_request_user_data/log_request_user_data.ts
+++ netlify/functions/log_request_user_data/log_request_user_data.ts
@@ -1,12 +1,52 @@
-import { Handler } from '@netlify/functions'
+const faunadb = require('faunadb');
+exports.handler = async (event) => {
+  const q = faunadb.query;
+  const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET_KEY,
+  });
 
-export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  const { slug } = event.queryStringParameters;
+  if (!slug) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Article slug not provided',
+      }),
+    };
+  }
+
+  const doesDocExist = await client.query(
+    q.Exists(q.Match(q.Index('likes_by_slug'), slug))
+  );
+  
+  if (!doesDocExist) {
+    await client.query(
+      q.Create(q.Collection('likes'), {
+        data: { slug: slug, likes: 1 },
+      })
+    );
+  }
+
+  const document = await client.query(
+    q.Get(q.Match(q.Index('likes_by_slug'), slug))
+  );
+
+  await client.query(
+    q.Update(document.ref, {
+      data: {
+        likes: document.data.likes + 1,
+      },
+    })
+  );
+
+  const updatedDocument = await client.query(
+    q.Get(q.Match(q.Index('likes_by_slug'), slug))
+  );
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      likes: updatedDocument.data.likes,
     }),
-  }
-}
+  };
+};
