525339b2d622d3963f67177fc369583963996229
Matthias Lischka <matthias.lischka@gmx.at>
2021-10-21 20:52:38 +0200

    log user request

--- netlify/functions/log_request_user_data/log_request_user_data.ts
+++ netlify/functions/log_request_user_data/log_request_user_data.ts
@@ -1,4 +1,4 @@
-const faunadb = require('faunadb');
+const faunadb = require("faunadb");
 exports.handler = async (event) => {
   const q = faunadb.query;
   const client = new faunadb.Client({
@@ -10,43 +10,16 @@ exports.handler = async (event) => {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Article slug not provided',
+        message: "slug not provided",
       }),
     };
   }
 
-  const doesDocExist = await client.query(
-    q.Exists(q.Match(q.Index('likes_by_slug'), slug))
-  );
-  
-  if (!doesDocExist) {
-    await client.query(
-      q.Create(q.Collection('likes'), {
-        data: { slug: slug, likes: 1 },
-      })
-    );
-  }
-
-  const document = await client.query(
-    q.Get(q.Match(q.Index('likes_by_slug'), slug))
-  );
-
   await client.query(
-    q.Update(document.ref, {
-      data: {
-        likes: document.data.likes + 1,
-      },
+    q.Create(q.Collection("request_logs"), {
+      data: { slug: slug, time_utc: new Date().getUTCDate() },
     })
   );
 
-  const updatedDocument = await client.query(
-    q.Get(q.Match(q.Index('likes_by_slug'), slug))
-  );
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      likes: updatedDocument.data.likes,
-    }),
-  };
+  return { statusCode: 200 };
 };
