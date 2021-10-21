ab59155114986d345f165dfc854ef8274faa1112
Matthias Lischka <matthias.lischka@gmx.at>
2021-10-21 22:18:24 +0200

    log context and event

--- netlify/functions/log_request_user_data/log_request_user_data.ts
+++ netlify/functions/log_request_user_data/log_request_user_data.ts
@@ -1,11 +1,18 @@
 const faunadb = require("faunadb");
-import fetch from "node-fetch";
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const q = faunadb.query;
   const client = new faunadb.Client({
     secret: process.env.FAUNA_SECRET_KEY,
   });
+
+  console.log("event: ");
+  console.log(event);
+
+  console.log("userinfo:");
+  console.log("IP:" + getHeaderInfo(event, "x-bb-ip"));
+  console.log("User Agent: " + getHeaderInfo(event, "user-agent"));
+  console.log("Country: " + getHeaderInfo(event, "x-country"));
 
   const { slug } = event.queryStringParameters;
   if (!slug) {
@@ -17,13 +24,20 @@ exports.handler = async (event) => {
     };
   }
 
-  let userData = await fetch("https://ipinfo.io/?token=" + process.env.IP_INFO_TOKEN);
-
   await client.query(
     q.Create(q.Collection("request_logs"), {
-      data: userData,
+      data: {
+        ip: getHeaderInfo(event, "x-bb-ip"),
+        useragent: getHeaderInfo(event, "user-agent"),
+        country: getHeaderInfo(event, "x-country"),
+        time: new Date().toISOString(),
+      },
     })
   );
 
   return { statusCode: 200 };
 };
+
+function getHeaderInfo(event, key) {
+  if (event && event.headers) return event.headers[key];
+}
